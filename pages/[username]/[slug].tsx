75a4ae4d300a9030dece449cafd664c3c92e854c
Deece94 <Dcollar@hotmail.com.au>
2022-06-29 17:58:19 +1000

    added basic routing

---
+++ pages/[username]/[slug].tsx
@@ -0,0 +1,11 @@
+
+
+export default function PostPage({}) {
+    return (
+        <main>
+            <h1>
+                Admin
+            </h1>
+        </main>
+    )
+}
---
+++ pages/[username]/index.tsx
@@ -0,0 +1,11 @@
+
+
+export default function UserPage({}) {
+    return (
+        <main>
+            <h1>
+                User
+            </h1>
+        </main>
+    )
+}
