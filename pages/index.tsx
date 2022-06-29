c77df3c3bac4337cb33bb9e056e13e4c32395b6a
Deece94 <Dcollar@hotmail.com.au>
2022-06-29 17:58:13 +1000

    added basic routing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,18 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div>
-
+    <div> 
+      <Link 
+        prefetch={true}
+        href={{
+          pathname: "/[username]",
+          query: {username: 'dylanc'}
+        }}>
+        <a>Dylans Profile</a>
+      </Link>
     </div>
   )
 }
